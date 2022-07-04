60b06814e13876f4065390be26813c1ce169851d
emmanuel okuchukwu <emmanuelokuchukwu@emmanuels-MacBook-Pro.local>
2022-07-04 11:50:47 +0100

    exercise 11.13

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get('/health', (_, res) => {
   res.json('Ok')
 })
 
+app.get('/version', (_, res) => {
+  res.send('1')
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port: ${PORT}`)
